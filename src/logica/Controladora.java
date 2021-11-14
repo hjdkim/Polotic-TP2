d623e12110c9e8357533634ca1f574402d27b9e9
HJD Kim <hjdkim@gmail.com>
2021-11-14 19:01:29 -0300

    Agregada la clase de igu

--- src/logica/Controladora.java
+++ src/logica/Controladora.java
@@ -3,11 +3,24 @@ package logica;
 import persistencia.ControladoraPersistencia;
 
 public class Controladora {
-    
+
     ControladoraPersistencia controlPersis = new ControladoraPersistencia();
-    
-    public void crear(Cliente cli){
+
+    public void crearCliente(Cliente cli) {
         controlPersis.crearCliente(cli);
     }
-    
+
+    public void crearCliente(String num_cliente, String nombre_perro, String raza, String color, Boolean alergico, Boolean atencion_especial, String nombre_duenio, String celular_duenio, String observaciones) {
+        Cliente cli = new Cliente();
+        cli.setNum_cliente(num_cliente);
+        cli.setNombre_perro(nombre_perro);
+        cli.setRaza(raza);
+        cli.setColor(color);
+        cli.setAlergico(alergico);
+        cli.setAtencion_especial(atencion_especial);
+        cli.setNombre_duenio(nombre_duenio);
+        cli.setCelular_duenio(celular_duenio);
+        cli.setObservaciones(observaciones);
+    }
+
 }
--- src/logica/TPO2.java
+++ src/logica/TPO2.java
@@ -1,17 +1,23 @@
 package logica;
 
+import igu.PantallaPrincipal;
+
 public class TPO2 {
 
     public static void main(String[] args) {
         
         Controladora control = new Controladora();
+        PantallaPrincipal panta = new PantallaPrincipal(control);
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
         
         /*Cliente prueba
         Cliente cliente1 = new Cliente(0, "1", "Toby", "Yorkshire terrier", "Negro", Boolean.FALSE, Boolean.TRUE, "Daniel", "1134330220", "Sin observaciones");
         */
         
         /*Create prueba
-        control.crear(cliente1);
+        control.crearCliente(cliente1);
         */
     }
     
