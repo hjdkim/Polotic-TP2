67d51e2ded7926bba37aae33a1568fa0467554de
HJD Kim <hjdkim@gmail.com>
2021-11-15 01:45:17 -0300

    TPO2 terminado

--- src/logica/TPO2.java
+++ src/logica/TPO2.java
@@ -10,14 +10,7 @@ public class TPO2 {
         PantallaPrincipal panta = new PantallaPrincipal(control);
         panta.setVisible(true);
         panta.setLocationRelativeTo(null);
-        
-        /*Cliente prueba
-        Cliente cliente1 = new Cliente(0, "1", "Toby", "Yorkshire terrier", "Negro", Boolean.FALSE, Boolean.TRUE, "Daniel", "1134330220", "Sin observaciones");
-        */
-        
-        /*Create prueba
-        control.crearCliente(cliente1);
-        */
+
     }
     
 }
